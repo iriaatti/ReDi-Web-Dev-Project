1256bb891be6866602bd1ea1e2bf9f6b24f6e88a
iriaatti <iriaatticiati@gmail.com>
2025-05-16 20:05:12 +0200

    created functions to generate deck of cards added list of emojis

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let card = [];
+let emojis = ["🦜", "🪼", "🐢", "🐈", "🐘"];
+let deckOfCards = [];
 
 class Cards {
   constructor(emoji, flipStatus) {
@@ -8,4 +9,30 @@ class Cards {
   flip() {
     this.flipStatus = !this.flipStatus;
   }
+  
 }
+
+//copied from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+function generateCards() {
+  emojis.forEach(function (emoji) {
+    let card = new Cards(emoji, false);
+    let card2 = new Cards(emoji, false);
+    deckOfCards.push(card);
+    deckOfCards.push(card2);
+  });
+  shuffleArray(deckOfCards);
+  console.log(deckOfCards);
+}
+
+let playButton = document.getElementById("play-button");
+
+playButton.addEventListener("click", generateCards);
