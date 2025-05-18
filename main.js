55be21e62f13f8fbdc1ddca0e40e4f3ae3c56fdc
iriaatti <iriaatticiati@gmail.com>
2025-05-18 13:17:50 +0200

    created functions to generate html cards

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ class Cards {
   flip() {
     this.flipStatus = !this.flipStatus;
   }
-  
 }
 
 //copied from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -23,13 +22,31 @@ function shuffleArray(array) {
 }
 
 function generateCards() {
+  deckOfCards = [];
+  let gameTable = document.getElementsByClassName("table")[0];
+  gameTable.innerHTML = "";
   emojis.forEach(function (emoji) {
     let card = new Cards(emoji, false);
     let card2 = new Cards(emoji, false);
     deckOfCards.push(card);
     deckOfCards.push(card2);
   });
+
   shuffleArray(deckOfCards);
+  deckOfCards.forEach(function (card) {
+    let cardDiv = document.getElementsByClassName("card")[0];
+    let cloneCard = cardDiv.cloneNode(true);
+    let cardFront = cloneCard.getElementsByClassName("cardFront")[0];
+
+    let cardBase = cloneCard.getElementsByClassName("cardBase")[0];
+    cardBase.addEventListener("click", function () {
+      cardBase.style.transform = "rotateY(-180deg)";
+    });
+
+    let emojiNode = document.createTextNode(card.emoji);
+    cardFront.appendChild(emojiNode);
+    gameTable.appendChild(cloneCard);
+  });
   console.log(deckOfCards);
 }
 
